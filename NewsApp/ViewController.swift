80a924e6d2a2d1f2477fff0b6b7de64f5719af4c
Dmitriy Lupych <dmitriy.lupych@softermii.com>
2019-07-08 13:38:30 +0300

    Initial Commit

---
+++ NewsApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NewsApp
+//
+//  Created by Dmitriy Lupych on 7/8/19.
+//  Copyright © 2019 Dmitry Lupich. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
